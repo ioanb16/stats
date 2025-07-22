ee06c53ae8eab478c7a081706a2c667c78864750
ioanb16 <barberioan6@gmail.com>
2025-07-22 13:35:28 +0100

    we have added a function that finds the median

--- stats_functions.py
+++ stats_functions.py
@@ -1,2 +1,11 @@
 def mean(a_list):
     return sum(a_list)/len(a_list)
+
+def med(a_list)
+    sorted_list = sorted(a_list)
+    if len(sorted_list)%2 ==1:
+	middle = len(sorted_list)/2
+	return sorted_list[middle]
+    m1 = (len(sorted_list)-1)/2
+    m2 = (len(sorted_list)+1/2
+    return (sorted_list[m1] + sorted_list[m2])/2
